22a6677212bffed4af09a050e4163ad9a23dfb80
John Biddulph <john.mbiddulph@gmail.com>
2020-04-23 19:04:47 +0100

    testing webhook should be working now

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -13,7 +13,7 @@
         <div class="row">
             <div class="col-md-4">
                 <a href="{{route('allproperties')}}">
-                    <button class="btn btn-secondary btn-lg mb-2" style="width: 100%;">Back to all Propertiessz</button>
+                    <button class="btn btn-secondary btn-lg mb-2" style="width: 100%;">Back to all Propertiesszzz</button>
                 </a>
                 <div class="card">
                     @if(isset($property->propimage))
