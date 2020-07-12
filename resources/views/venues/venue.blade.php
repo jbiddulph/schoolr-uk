d2a715439686fa7f086cc0027281b5c0e68c4497
John Biddulph <john.mbiddulph@gmail.com>
2020-07-12 17:29:38 +0100

    sorted covid banner

--- resources/views/venues/venue.blade.php
+++ resources/views/venues/venue.blade.php
@@ -34,13 +34,13 @@
             </div>
             <div class="col-md-9">
 
-                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <div class="alert alert-outline-danger alert-dismissible fade show" role="alert">
                         <strong>Covid-19 Lockdown notice</strong> Due to the current lockdown and social distancing measures that are in place to help save lives, venues and pubs will have a limited amount of events listed.
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                ß
+
                 @foreach($events as $event)
                     {{$event->eventName}}
                 @endforeach
