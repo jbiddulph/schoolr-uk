43dd99f8795c04d207329bbbcbf3fb21bfe9d6f9
John Biddulph <john.mbiddulph@gmail.com>
2020-08-31 08:40:30 +0100

    changed header

--- resources/views/register-landlord.blade.php
+++ resources/views/register-landlord.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="colorbar"></div>
-<img src="{{asset('/cover/seaside_header.jpg')}}" style="width: 100%;" alt="Seaside sussex">
+<img src="{{asset('/cover/bar_header.jpg')}}" style="width: 100%;" alt="Bar Header">
     <div class="container mt-4">
         <div class="row justify-content-center">
             <div class="col-md-8">
