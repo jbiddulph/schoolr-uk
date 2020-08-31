66e92a289ccdae62a0cd34ec81a1fa3860ad5609
John Biddulph <john.mbiddulph@gmail.com>
2020-08-31 23:41:26 +0100

    sorted homepage photos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
                     <div class="property-card card mb-4">
                         @if(isset($venue->photo))
                             @php
-                                $mainphoto = str_replace('public/venues/', 'storage/', $venue->photo)
+                                $mainphoto = str_replace('public/venues/', 'storage/venues/photos/', $venue->photo)
                             @endphp
                             <div class="mainpic">
                                 <a href="{{route('properties.show',[$venue->id, $venue->slug])}}"><img class="d-block img-fluid prop_photo" src="/{{ $mainphoto }}" alt="{{$venue->venuename}}"></a>
