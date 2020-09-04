bbd308ac0eb5cd74e8cb8ac52ee68f0eb23750a0
John Biddulph <john.mbiddulph@gmail.com>
2020-09-04 23:51:05 +0100

    spacing

--- resources/views/venues/tagin.blade.php
+++ resources/views/venues/tagin.blade.php
@@ -63,7 +63,7 @@
                             </div>
                             <div class="row">
                                 <div class="col-md-4 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">Tag in!</button>
+                                    <button type="submit" class="btn bt-lg btn-primary">Tag in!</button>
                                 </div>
                             </div>
                         </form>
