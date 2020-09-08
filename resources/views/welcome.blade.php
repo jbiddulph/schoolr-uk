0ab8dc6ba820dbc233a1cdd2b7368771486e8f4d
John Biddulph <john.mbiddulph@gmail.com>
2020-09-08 20:13:21 +0100

    testing storage folder link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
         </div>
         <div class="row">
             <div class="col-md-4">
-
+                <a href="{{url('storage/app/letters/Southwick/TheCricketers.pdf')}}">storage/app/letters/Southwick/TheCricketers.pdf</a>
             </div>
             <div class="col-md-4">
 
