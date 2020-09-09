4065ac05d9d2b4c9c53009d9b9bbceb485805aa9
Solution17 <solution17@solution17s-air.home>
2020-09-09 21:05:44 +0100

    Check if valid link

--- resources/views/administration/adminvenue.blade.php
+++ resources/views/administration/adminvenue.blade.php
@@ -36,7 +36,7 @@
                                     {{$venue->town}}
                                 </p>
                                 </a>
-                                @if(is_link(Storage::url('letters/'. $venue->town .'/'.$venue->venuename.'.pdf')))
+                                @if(asset::url('letters/'. $venue->town .'/'.$venue->venuename.'.pdf'))
                                     <a href="{{Storage::url('letters/'. $venue->town .'/'.$venue->venuename.'.pdf')}}">View Letter</a>
                                 @endif
                             </div>
