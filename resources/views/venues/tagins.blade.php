ee6b68de45f323685f65a1ca1377e677c716077b
John Michael <johnmichael@s-macbook-pro.home>
2020-09-23 21:17:30 +0100

    View tagins by cards or table

--- resources/views/venues/tagins.blade.php
+++ resources/views/venues/tagins.blade.php
@@ -30,33 +30,69 @@
                         @endforeach
                     </ul>
                 </div>
+                <ul class="nav nav-tabs mt-4" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Card View</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Table View</a>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        @foreach($tagins as $tagin)
+                            <div class="tagincards mt-4 welcome">
+
+{{--                                <div class="col-md-2 col-sm-12">--}}
+                                    <div class="venue-card card mb-12">
+                                        <div class="card-body">
+                                            {{--                        <h4 class="card-title"><a href="{{route('venues.show',[$venue->id, $venue->slug])}}">{{$venue->propname}}</a></h4>--}}
+
+                                            <div class="card-text">
+                                                <div class="phonenumber">{{$tagin->phone_number}}</div>
+                                                {{--                            <a class="btn btn-primary btn-sm" href="{{route('properties.show',[$venue->id, $venue->slug])}}">Enquire</a>--}}
+                                                <div class="emailaddress">{{$tagin->email_address}}</div>
+                                                {{--                            <a class="btn btn-primary btn-sm" href="{{route('properties.show',[$venue->id, $venue->slug])}}">Enquire</a>--}}
+                                                <div class="reason">{{$tagin->reason_visit}}</div>
+                                                <div class="marketing">{{$tagin->marketing}}</div>
+                                                <div class="address">{{$tagin->created_at}}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+{{--                                </div>--}}
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                        <table class="table table-striped mt-4">
+                            <thead>
+                            <tr>
+                                <th>Phone number</th>
+                                <th>Email Address</th>
+                                <th>Reason for Visit</th>
+                                <th>Marketing</th>
+                                <th>Tagged in at</th>
+                            </tr>
+                            </thead>
+                            @foreach($tagins as $tagin)
+                            <tr>
+                                <td><div class="phonenumber">{{$tagin->phone_number}}</div></td>
+                                            {{--                            <a class="btn btn-primary btn-sm" href="{{route('properties.show',[$venue->id, $venue->slug])}}">Enquire</a>--}}
+                                <td><div class="emailaddress">{{$tagin->email_address}}</div></td>
+                                            {{--                            <a class="btn btn-primary btn-sm" href="{{route('properties.show',[$venue->id, $venue->slug])}}">Enquire</a>--}}
+                                <td><div class="reason">{{$tagin->reason_visit}}</div></td>
+                                <td><div class="marketing">{{$tagin->marketing}}</div></td>
+                                <td><div class="address">{{$tagin->created_at}}</div></td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="container mt-4 welcome">
-        <div class="row">
-            @foreach($tagins as $tagin)
-                <div class="col-md-2 col-sm-12">
-                    <div class="venue-card card mb-12">
-                        <div class="card-body">
-                            {{--                        <h4 class="card-title"><a href="{{route('venues.show',[$venue->id, $venue->slug])}}">{{$venue->propname}}</a></h4>--}}
 
-                            <div class="card-text">
-                                <div class="phonenumber">{{$tagin->phone_number}}</div>
-                            {{--                            <a class="btn btn-primary btn-sm" href="{{route('properties.show',[$venue->id, $venue->slug])}}">Enquire</a>--}}
-                                <div class="emailaddress">{{$tagin->email_address}}</div>
-                                {{--                            <a class="btn btn-primary btn-sm" href="{{route('properties.show',[$venue->id, $venue->slug])}}">Enquire</a>--}}
-                                <div class="reason">{{$tagin->reason_visit}}</div>
-                                <div class="marketing">{{$tagin->marketing}}</div>
-                                <div class="address">{{$tagin->created_at}}</div>
-                            </div>
-                     </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
     <div class="colorbar mt-5"></div>
     @endif
 @endsection
