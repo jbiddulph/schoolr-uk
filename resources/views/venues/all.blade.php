0bc40dd897afcc312a05e8b8b852747a629e2508
John Michael <johnmichael@s-macbook-pro.home>
2020-10-08 18:56:02 +0100

    fixing page

--- resources/views/venues/all.blade.php
+++ resources/views/venues/all.blade.php
@@ -26,44 +26,44 @@
             </ul>
         </div>
         <div class="row">
-            @foreach($venueslist as $venue)
-                <div class="col-md-2 col-sm-12">
-                    <a href="/venues/{{ str_slug($venue->town)}}/{{str_slug($venue->school)}}/{{$venue->id}}">
-                        <div class="venue-card card mb-4">
-                        @if(isset($venue->photo))
-                            @php
-                                $mainphoto = str_replace('public/', 'storage/', $venue->photo)
-                            @endphp
-                            <div class="mainpic" style="background-image: url('/\{{ $mainphoto }}'); background-repeat: no-repeat;     background-size: cover;
-                                display: block;
-                                width: 100%;
-                                height: 120px;">
+{{--            @foreach($venueslist as $venue)--}}
+{{--                <div class="col-md-2 col-sm-12">--}}
+{{--                    <a href="/venues/{{ str_slug($venue->town)}}/{{str_slug($venue->school)}}/{{$venue->id}}">--}}
+{{--                        <div class="venue-card card mb-4">--}}
+{{--                        @if(isset($venue->photo))--}}
+{{--                            @php--}}
+{{--                                $mainphoto = str_replace('public/', 'storage/', $venue->photo)--}}
+{{--                            @endphp--}}
+{{--                            <div class="mainpic" style="background-image: url('/\{{ $mainphoto }}'); background-repeat: no-repeat;     background-size: cover;--}}
+{{--                                display: block;--}}
+{{--                                width: 100%;--}}
+{{--                                height: 120px;">--}}
 
-                                    {{--                                    <img class="d-block img-fluid prop_photo" src="/{{ $mainphoto }}" alt="{{$venue->school}}" width="180">--}}
+{{--                                    --}}{{--                                    <img class="d-block img-fluid prop_photo" src="/{{ $mainphoto }}" alt="{{$venue->school}}" width="180">--}}
 
-                                <h2 class="card-subtitle">{{$venue->school}}</h2>
+{{--                                <h2 class="card-subtitle">{{$venue->school}}</h2>--}}
 
-                            </div>
-                        @endif
-                        <div class="card-body">
-                            {{--                        <h4 class="card-title"><a href="{{route('venues.show',[$venue->id, $venue->slug])}}">{{$venue->propname}}</a></h4>--}}
+{{--                            </div>--}}
+{{--                        @endif--}}
+{{--                        <div class="card-body">--}}
+{{--                            --}}{{--                        <h4 class="card-title"><a href="{{route('venues.show',[$venue->id, $venue->slug])}}">{{$venue->propname}}</a></h4>--}}
 
-                            <div class="card-text">
-                                <div class="address_1">{{$venue->address_1}}<br />
-                                    @if($venue->address_2 != '')
-                                        {{$venue->address_2}}<br />
-                                    @endif
-                                    {{$venue->town}}<br />
-                                    {{$venue->county}}<br />
-                                    {{$venue->postcode}}</div>
-                                <span><a href="tel:{{$venue->telephone}}"><i class="fas fa-2x fa-phone-alt"></i></a></span>
-                            </div>
-                            {{--                            <a class="btn btn-primary btn-sm" href="{{route('properties.show',[$venue->id, $venue->slug])}}">Enquire</a>--}}
-                        </div>
-                    </div>
-                    </a>
-                </div>
-            @endforeach
+{{--                            <div class="card-text">--}}
+{{--                                <div class="address_1">{{$venue->address_1}}<br />--}}
+{{--                                    @if($venue->address_2 != '')--}}
+{{--                                        {{$venue->address_2}}<br />--}}
+{{--                                    @endif--}}
+{{--                                    {{$venue->town}}<br />--}}
+{{--                                    {{$venue->county}}<br />--}}
+{{--                                    {{$venue->postcode}}</div>--}}
+{{--                                <span><a href="tel:{{$venue->telephone}}"><i class="fas fa-2x fa-phone-alt"></i></a></span>--}}
+{{--                            </div>--}}
+{{--                            --}}{{--                            <a class="btn btn-primary btn-sm" href="{{route('properties.show',[$venue->id, $venue->slug])}}">Enquire</a>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    </a>--}}
+{{--                </div>--}}
+{{--            @endforeach--}}
         </div>
         <div class="row justify-content-center">
             <div class="offset-md-2 col-md-8 text-center">
